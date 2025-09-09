77af82ea97fddd9af7f67b540be4384bf5ab5d7c
Hannes Lowette <lowette.hannes@gmail.com>
2025-09-09 09:21:54 +0200

    Exercise on Projections

---
+++ src/BeerSender.Domain/Projections/BeerInBoxes.cs
@@ -0,0 +1,16 @@
+﻿using BeerSender.Domain.Boxes;
+using Marten.Events.Projections;
+
+namespace BeerSender.Domain.Projections;
+
+public class BeerInBoxes
+{
+    public required string BottleType { get; set; }
+    public required BeerBottle Bottle { get; set; }
+    public List<Guid> BoxIds { get; set; } = new();
+}
+
+public class BeerInBottlesProjection : MultiStreamProjection<BeerInBoxes, string>
+{
+    //TODO
+}
---
+++ src/BeerSender.Domain/Projections/UnsentBoxes.cs
@@ -0,0 +1,25 @@
+﻿using BeerSender.Domain.Boxes.Events;
+using JasperFx.Events;
+using Marten.Events.Projections;
+
+namespace BeerSender.Domain.Projections;
+
+public class UnsentBox
+{
+    public Guid BoxId { get; set; }
+    public string? Status { get; set; }
+}
+
+public class UnsentBoxProjection : EventProjection
+{
+    public UnsentBoxProjection()
+    {
+        Project<IEvent<BoxCreated>>((evt, operations) =>
+        {
+            operations.Store(new UnsentBox
+            {
+                BoxId = evt.StreamId
+            });
+        });
+    }
+}
